24cc4cf92da6106feacba62767c088d4a34ca42f
dimit <dimitar_rradev@abv.bg>
2025-04-02 14:18:16 +0300

    added wiremock dependency for offline test of GlobalExcepttionHandler

--- src/test/java/bg/softuni/serviceScheduler/web/GlobalExceptionHandlerIT.java
+++ src/test/java/bg/softuni/serviceScheduler/web/GlobalExceptionHandlerIT.java
@@ -2,6 +2,8 @@ package bg.softuni.serviceScheduler.web;
 
 import bg.softuni.serviceScheduler.user.dao.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,15 +13,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.wiremock.spring.ConfigureWireMock;
+import org.wiremock.spring.EnableWireMock;
+import org.wiremock.spring.InjectWireMock;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@EnableWireMock(
+        @ConfigureWireMock(
+                name = "service-scheduler-car-brands-and-models",
+                port = (8081)
+        )
+)
 public class GlobalExceptionHandlerIT {
 
+    @InjectWireMock("service-scheduler-car-brands-and-models")
+    private WireMockServer wireMockServer;
     @Autowired
     private MockMvc mockMvc;
     @Autowired
@@ -67,12 +82,14 @@ public class GlobalExceptionHandlerIT {
 
     @Test
     public void testHandleCarModelAddException() throws Exception {
-        MockHttpServletRequestBuilder post = MockMvcRequestBuilders
-                .post("/models/add/" + "Audi")
-                .param("brandName", "Audi")
-                .param("model", "A4")
-                .with(user(authorizationService.getUserDetailsAdmin()))
-                .with(csrf());
+        wireMockServer.stubFor(post("/models/add/" + "Audi").willReturn(WireMock.status(409)));
+
+                MockHttpServletRequestBuilder post = MockMvcRequestBuilders
+                        .post("/models/add/" + "Audi")
+                        .param("brandName", "Audi")
+                        .param("model", "A4")
+                        .with(user(authorizationService.getUserDetailsAdmin()))
+                        .with(csrf());
 
         mockMvc.perform(post)
                 .andExpect(status().isConflict());
@@ -87,6 +104,5 @@ public class GlobalExceptionHandlerIT {
                 .param("password", "validpassword")
                 .param("confirmPassword", "validpassword");
     }
-
 
 }
